4ac86d7ca97843b111b337b4821c67ed65871c5b
Lyle Harris <lyle.harris@gcpay.com>
2025-11-23 19:32:53 -0800

    Define hashCode fields

--- src/main/java/org/gpc4j/web/dto/ScheduledClass.java
+++ src/main/java/org/gpc4j/web/dto/ScheduledClass.java
@@ -2,6 +2,7 @@ package org.gpc4j.web.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import org.gpc4j.web.api.ClassType;
 import org.gpc4j.web.security.UserAccount;
@@ -14,12 +15,23 @@ import java.time.LocalDateTime;
 public class ScheduledClass {
 
   private String id; // RavenDB document id (e.g., ClassOffering/1-A)
+
+  @EqualsAndHashCode.Include
   private String className;
+
+  @EqualsAndHashCode.Include
   private ClassType classType;
+
+  @EqualsAndHashCode.Include
   private String level;
+
+  @EqualsAndHashCode.Include
   private LocalDateTime start;
-  private int duration;
+
+  @EqualsAndHashCode.Include
   private String location;
+
+  private int duration;
   private int slots;
   private UserAccount instructorAccount;
 
