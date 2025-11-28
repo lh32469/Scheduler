a9b1c648f44a934c082671574769ff5127f9a516
Lyle T Harris <lyle.harris@gmail.com>
2025-11-28 10:06:17 -0800

    CSRF Changes

--- src/main/java/org/gpc4j/web/security/SecurityConfig.java
+++ src/main/java/org/gpc4j/web/security/SecurityConfig.java
@@ -12,6 +12,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 @Configuration
 @EnableWebSecurity
@@ -32,7 +33,10 @@ public class SecurityConfig {
   @Bean
   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
     http
-        .csrf(csrf -> csrf.ignoringRequestMatchers("/actuator/**"))
+        .csrf(csrf ->
+                  csrf.ignoringRequestMatchers("/actuator/**"))
+        .csrf(csrf ->
+                  csrf.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()))
         .authorizeHttpRequests(auth -> auth
             .requestMatchers(
                 request ->
