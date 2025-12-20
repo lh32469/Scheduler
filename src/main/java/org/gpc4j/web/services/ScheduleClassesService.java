a8276052b028df6a120757ced0d63a07a22a7330
Lyle T Harris <lyle.harris@gmail.com>
2025-12-20 14:34:37 -0800

    Filter scheduled classes by query params if present.

--- src/main/java/org/gpc4j/web/services/ScheduleClassesService.java
+++ src/main/java/org/gpc4j/web/services/ScheduleClassesService.java
@@ -2,12 +2,14 @@ package org.gpc4j.web.services;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import net.ravendb.client.documents.session.IDocumentQuery;
 import net.ravendb.client.documents.session.IDocumentSession;
 import org.gpc4j.web.dto.ClassSchedule;
 import org.gpc4j.web.dto.ScheduledClass;
 import org.gpc4j.web.repository.RavenDB;
 import org.gpc4j.web.security.UserAccount;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 
 import java.time.LocalDate;
 import java.util.Comparator;
@@ -23,17 +25,30 @@ public class ScheduleClassesService {
   private final RavenDB ravenDB;
 
   public List<ScheduledClass> getScheduledClasses(LocalDate startDate,
-                                                  LocalDate endDate) {
+                                                  LocalDate endDate,
+                                                  String classType,
+                                                  String instructorId) {
 
     List<ScheduledClass> classes = new LinkedList<>();
 
     try (IDocumentSession session = ravenDB.openSession()) {
 
-      // Get all the Schedules
-      List<ClassSchedule> schedules = session
-          .query(ClassSchedule.class)
-          .include("instructorId")
-          .toList();
+      // Start the Query
+      IDocumentQuery<ClassSchedule> query =
+          session.query(ClassSchedule.class)
+                 .include("instructorId");
+
+      if (StringUtils.hasText(classType)) {
+        log.debug("Filtering classes by class type " + classType);
+        query.whereEquals("classType", classType.trim());
+      }
+
+      if(StringUtils.hasText(instructorId)) {
+        log.debug("Filtering classes by instructorId " + instructorId);
+        query.whereEquals("instructorId", instructorId.trim());
+      }
+
+      List<ClassSchedule> schedules = query.toList();
 
       schedules.forEach(schedule -> {
         List<ScheduledClass> scheduledClasses =
@@ -55,8 +70,10 @@ public class ScheduleClassesService {
     }
 
     classes.sort(Comparator.comparing(ScheduledClass::getStart));
-    log.debug("Found " + classes.size() + " classes for "
-                 + startDate + " to " + endDate);
+    log.info("Found " + classes.size() + " classes for "
+                 + startDate + " to " + endDate
+                 + " for classType " + classType
+                 + " and instructorId " + instructorId);
 
     return classes;
   }
