f0116ce6eafb5d8c4755f4f0630085bd4dcd2236
Lyle T Harris <lyle.harris@gmail.com>
2025-12-21 13:25:51 -0800

    Renamed @SpringBootApplication

--- src/test/java/org/gpc4j/web/SchedulerApplicationTests.java
+++ src/test/java/org/gpc4j/web/SchedulerApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class WebTemplateApplicationTests {
+class SchedulerApplicationTests {
 
   @Test
   void contextLoads() {
