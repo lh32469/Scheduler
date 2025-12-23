0e23222a8898c8c796b48c652f8b8d6f828d0ec4
Lyle T Harris <lyle.harris@gmail.com>
2025-12-23 12:20:29 -0800

    Cache style.css in browser

---
+++ src/main/java/org/gpc4j/web/configs/WebConfig.java
@@ -0,0 +1,22 @@
+package org.gpc4j.web.configs;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.CacheControl;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.concurrent.TimeUnit;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+  @Override
+  public void addResourceHandlers(ResourceHandlerRegistry registry) {
+    registry
+        .addResourceHandler("/styles.css")
+        .addResourceLocations("classpath:/static/")
+        .setCacheControl(CacheControl.maxAge(1, TimeUnit.DAYS)
+                                     .cachePublic());
+  }
+
+}
