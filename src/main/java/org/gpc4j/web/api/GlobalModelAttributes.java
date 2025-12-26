85a7d591e9d72d531b476fe77a7ae2ca2cfafbb6
Lyle T Harris <lyle.harris@gmail.com>
2025-12-26 13:46:49 -0800

    fixup

--- src/main/java/org/gpc4j/web/api/GlobalModelAttributes.java
+++ src/main/java/org/gpc4j/web/api/GlobalModelAttributes.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ModelAttribute;
 
 @Slf4j
-@ControllerAdvice(basePackages = {"org.gpc4j.web.controllers", "org.gpc4j.web.api"})
+@ControllerAdvice(basePackages = {"org.gpc4j.web"})
 @RequiredArgsConstructor
 public class GlobalModelAttributes {
 
